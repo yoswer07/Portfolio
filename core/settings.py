25f9d83aa3167be3360c8e0ff863092d8fa40127
yoswer07 <yoswer07@gmail.com>
2023-06-22 14:02:23 -0400

    Configuracion de react

--- core/settings.py
+++ core/settings.py
@@ -82,7 +82,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
